0758bbb07f86d44033b09df30ea77c0818cd3706
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2023-12-04 13:40:55 +0530

    display sgn

---
+++ displaySign.js
@@ -0,0 +1,31 @@
+var x  = 3;
+var y= -7;
+var z = 2;
+if (x>0 && y>0 && z>0)
+{
+    alert("this sign is  +");
+}
+else if (x< 0 && y<0 && z<0)
+    {
+        console.log("this sign is -");
+    }
+    else if (x> 0 && y<0 && z<0)
+    {
+        console.log("this sign is +");
+    }
+    else if (x<0 && y<0 && z<0)
+            {
+                console.log("this sign is -");
+            }
+            else if (x>0 && y<0 && z<0)
+            {
+                console.log("this sign is +");
+            }
+            else if ( x<0 && y>0 && z<0)
+            {
+                console.log("This sign is +");
+            }
+            else 
+            {
+                console.log("This sign is -");
+            }
